e572ae6ed83470402a35efd1e9a300dba63c57ce
rob grace <zwergie69@protonmail.com>
2023-04-20 14:47:24 +0200

    user fixture greyed out

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -1,24 +1,24 @@
 <?php
 
 namespace App\DataFixtures;
-use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
-use App\Entity\User;
+// use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+// use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
 class UserFixtures extends Fixture
 {
-    public function _construct(UserPasswordHasherInterface $hasher)
-    {
-        $this->hasher = $hasher;
-    }
+    // public function _construct(UserPasswordHasherInterface $hasher)
+    // {
+    //     $this->hasher = $hasher;
+    // }
     public function load(ObjectManager $manager): void
     {
-        $user = new User();
-        $user->setEmail("user@gmail.com");
-        $password = $this->hasher->hashPassword($user, '123456');
-        $user->setPassword($password);
-        $manager->persist($user);
+        // $user = new User();
+        // $user->setEmail("user@gmail.com");
+        // $password = $this->hasher->hashPassword($user, '123456');
+        // $user->setPassword($password);
+        // $manager->persist($user);
         $manager->flush();
     }
 }
